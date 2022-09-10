72201801c6926e5eaaecbcbe14369aaa47dac0d1
MOUSBAH <alloushmousbah1999@gmail.com>
2022-09-10 16:58:09 +0300

    creat-url-to-fetch-from

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,13 @@
+// in this function we need to fetch all data we need when the user click the button
+function fetchingData(){
+    let userName=document.getElementById("user-name").ariaValueMax;
 
+    // define all url we need to fetch from 
+    let genderUrl= "https://api.genderize.io?name="+userName
+    let ageUrl= "https://api.agify.io/?name="+userName
+    let nationalityUrl="https://api.nationalize.io/?name="+userName
+    
+    
+
+}
 
