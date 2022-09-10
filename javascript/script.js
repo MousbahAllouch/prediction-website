cd66780775b25c7cee9622a3e30e803dc3487354
MOUSBAH <alloushmousbah1999@gmail.com>
2022-09-10 17:49:15 +0300

    set-predicted-nationality

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 userGender=document.getElementById("user-gender");
 userAge=document.getElementById("user-age");
+userNationality=document.getElementById("user-nationality")
 
 
 // in this function we need to fetch all data we need when the user click the button
@@ -25,6 +26,17 @@ function fetchingData(){
         .then((data) => {
                 let age=data.age;
                 userAge.innerText=age;
+        });
+
+    // getting nationality and set it
+    fetch(nationalityUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            userNationality.innerText=data["country"][0]["country_id"]
+            // to check if ther are more than one country
+            if (data["country"]["length"]>1){
+                userNationality.innerText=data["country"][0]["country_id"]+" and "+data["country"][1]["country_id"]
+            }
         });
 
     
